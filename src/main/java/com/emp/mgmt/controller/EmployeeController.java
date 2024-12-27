a96666a6df68b0f4bc6cef3488543558f14f8c48
Codehunger <hungerforcode@gmail.com>
2024-12-28 00:54:08 +0530

    intial commit

---
+++ src/main/java/com/emp/mgmt/controller/EmployeeController.java
@@ -0,0 +1,10 @@
+package com.emp.mgmt.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EmployeeController {
+
+
+}
---
+++ src/test/java/com/emp/mgmt/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
